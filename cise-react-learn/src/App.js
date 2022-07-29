afae2cba2f4ad7911cbaee9591a55d49399e5090
Walter Siu <typist95>
2022-07-29 12:32:02 +1200

    Update content by Walter

--- cise-react-learn/src/App.js
+++ cise-react-learn/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Agile is about Values and Principles
+          Agile is about Values and Principles Updated by Walter
         </a>
       </header>
     </div>
